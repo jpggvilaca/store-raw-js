4a4ed8bf625e36e30f6f43b3de558726cd90ffd8
Tania Rascia <taniarascia@gmail.com>
2017-06-22 16:52:57 -0500

    Version 5

--- app/index.js
+++ app/index.js
@@ -1,55 +1,68 @@
 import './index.scss';
 
-// 1) create an array of objects (called items) with items in it, this is going to be your static data
-// 2) create an empty array called ‘cart’ and build functions to Add , edit, or remove items from it
-// 3) use console.log(var) to check values in your loops
-
 const store = {
-  items: [{
-      name: 'Bike Helmet',
-      color: 'Black',
-      price: 25
-    },
-    {
-      name: 'Pannier',
-      color: 'Black',
-      price: 50
-    },
-    {
-      name: 'Odometer',
-      color: 'Black',
-      price: 20
-    }
-  ],
-  createTable: function() {}
+	items: [
+		{
+			name: "Bike Helmet",
+			color: "Black",
+			price: 25
+		},
+		{
+			name: "Pannier",
+			color: "Black",
+			price: 50
+		},
+		{
+			name: "Odometer",
+			color: "Black",
+			price: 20
+		}
+	],
+	
+	createTable: function() {
+		let htmlTable = '';
+		for (let item of this.items) {
+		  htmlTable += 
+		  `<tr>
+  			<td>${item.name}</td>
+  			<td>${item.color}</td>
+  			<td>${item.price}</td>
+  		</tr>`
+		}
+		const storeTable = document.getElementById('store');
+		storeTable.innerHTML = htmlTable;
+	}
 };
 
 class CartController {
-  constructor() {
-    this.cart = [];
-  }
+	constructor() {
+		this.cart = [];
+	}
 
-  showItems = () => {
-    console.log('My Cart', this.cart);
-  }
+	showItems = () => {
+		console.log("My Cart", this.cart);
+	};
 
-  addItem = (position) => {
-    this.cart.push({
-      name: store.items[position].name,
-      color: store.items[position].color,
-      price: store.items[position].price
-    });
-    this.showItems();
-  }
+	addItem = position => {
+		this.cart.push({
+			name: store.items[position].name,
+			color: store.items[position].color,
+			price: store.items[position].price
+		});
+		this.showItems();
+	};
 
-  editItem = (position, newValue) => {
-    this.cart[position] = newValue;
-    this.showItems();
-  }
-  deleteItem = (position) => {
-    this.cart.splice(position, 1);
-    this.showItems();
-  }
+	editItem = (position, newValue) => {
+		this.cart[position] = newValue;
+		this.showItems();
+	};
+
+	deleteItem = position => {
+		this.cart.splice(position, 1);
+		this.showItems();
+	};
 }
 
 const myCart = new CartController();
+
+store.createTable();
