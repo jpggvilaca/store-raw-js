c2e0e641e8cb1361e8de766a1caf8709578c88bd
Tania Rascia <taniarascia@gmail.com>
2017-06-26 11:44:56 -0500

    Update

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ const store = {
 		},
 		{
 			name: "Pannier",
-			color: "Black",
+			color: "Purple",
 			price: 50
 		},
 		{
@@ -21,14 +21,27 @@ const store = {
 	
 	createTable: function() {
 		let htmlTable = '';
+		htmlTable += 
+		`<thead>
+      <tr>
+        <th>Name</th>
+        <th>Color</th>
+        <th>Price</th>
+        <th>Cart</th>
+      </tr>
+    </thead>
+		<tbody>`
 		for (let item of this.items) {
 		  htmlTable += 
 		  `<tr>
   			<td>${item.name}</td>
   			<td>${item.color}</td>
   			<td>${item.price}</td>
+  			<td>Add</td>
   		</tr>`
 		}
+		htmlTable +=
+		`</tbody>`
 		const storeTable = document.getElementById('store');
 		storeTable.innerHTML = htmlTable;
 	}
@@ -43,26 +56,52 @@ class CartController {
 		console.log("My Cart", this.cart);
 	};
 
-	addItem = position => {
+	addItem = index => {
 		this.cart.push({
-			name: store.items[position].name,
-			color: store.items[position].color,
-			price: store.items[position].price
+			name: store.items[index].name,
+			color: store.items[index].color,
+			price: store.items[index].price
 		});
 		this.showItems();
 	};
 
-	editItem = (position, newValue) => {
-		this.cart[position] = newValue;
+	editItem = (index, newValue) => {
+		this.cart[index] = newValue;
 		this.showItems();
 	};
 
-	deleteItem = position => {
-		this.cart.splice(position, 1);
+	deleteItem = index => {
+		this.cart.splice(index, 1);
 		this.showItems();
 	};
+
+	printCart = () => {
+		let htmlTable = '';
+		for (let item of this.cart) {
+			htmlTable +=
+			 `<tr>
+  			<td>${item.name}</td>
+  			<td>${item.color}</td>
+  			<td>${item.price}</td>
+  		</tr>`
+		}
+		const cartTable = document.getElementById('cart');
+		cartTable.innerHTML = htmlTable;
+	}
 }
 
 const myCart = new CartController();
 
+class MegaController {
+	constructor(store, cart) {
+	}
+}
+
+const controllEverything = new MegaController(store, cart);
+
+
 store.createTable();
+
+myCart.addItem(1);
+
+myCart.printCart();
